b418c1a6aec1aaab0865b43cd7570366367c6f16
nidhzzpatel <148216891+nidhzzpatel@users.noreply.github.com>
2024-06-30 21:39:10 +0530

    Delete BankCstudy directory

--- BankCstudy/BankFactory.java
+++
@@ -1,7 +0,0 @@
-package BankCstudy;
-
-public abstract class BankFactory {
-    public abstract SavingAcc getNewSavingAcc(int accNo, String accNm, float accBal, boolean isSalary);
-
-    public abstract CurrentAcc getNewCurrentAcc(int accNo, String accNm, float accBal, float creditLimit);
-}
